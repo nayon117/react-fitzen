44b36da681e73fff9ad96b739a9fa0647926ed83
nayon117 <hasibulhasannayon260@gmail.com>
2023-10-07 12:16:50 +0600

    add footer to the site

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,15 +1,21 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import { Toaster } from "react-hot-toast";
+import Footer from "../pages/Footer/Footer";
 
 const MainLayout = () => {
-    return (
-        <div className="container mx-auto px-4 md:px-8 lg:px-16 font-poppins">
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-            <Toaster/>
-        </div>
-    );
+  return (
+    <div className="">
+      <div className="container mx-auto px-4 md:px-8 lg:px-16 font-poppins">
+        <Navbar></Navbar>
+      </div>
+      <div className="min-h-screen container mx-auto px-4 md:px-8 lg:px-16">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+      <Toaster />
+    </div>
+  );
 };
 
 export default MainLayout;
