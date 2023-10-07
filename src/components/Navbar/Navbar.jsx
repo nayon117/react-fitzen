5a295a2850d11c001ede2841b7f9f77486bcdff1
nayon117 <hasibulhasannayon260@gmail.com>
2023-10-07 08:03:06 +0600

    add custom font and implement it to the site

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+    return (
+        <div>
+            i am navbar
+        </div>
+    );
+};
+
+export default Navbar;
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className="container mx-auto px-4 md:px-8 lg:px-16 font-poppins">
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
