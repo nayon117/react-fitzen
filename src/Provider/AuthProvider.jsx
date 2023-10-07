47ffc579737005019ba408f662afdda277f6b376
nayon117 <hasibulhasannayon260@gmail.com>
2023-10-07 09:22:08 +0600

    install firebase and implement config settings

---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+const AuthProvider = ({children}) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
+
+AuthProvider.propTypes = {
+    children:PropTypes.node
+};
+
+export default AuthProvider;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+ 
+const firebaseConfig = {
+  apiKey: "AIzaSyAFZWUbq8ILcQRvriZwFjZx2F9n17oND_w",
+  authDomain: "react-fitzen-a5d29.firebaseapp.com",
+  projectId: "react-fitzen-a5d29",
+  storageBucket: "react-fitzen-a5d29.appspot.com",
+  messagingSenderId: "843806875811",
+  appId: "1:843806875811:web:e68130ce5ed34f0ada1f16"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+export default auth;
