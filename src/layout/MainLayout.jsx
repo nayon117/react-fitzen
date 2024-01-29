c06001738d982f403ab0850b1f606d11e45be86d
nayon117 <hasibul.nayon1@gmail.com>
2024-01-29 12:35:10 +0600

    add chat feature

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,10 +2,16 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import { Toaster } from "react-hot-toast";
 import Footer from "../pages/Footer/Footer";
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 const MainLayout = () => {
   return (
     <div className="font-poppins">
+       <MessengerCustomerChat
+    pageId="211034232098177"
+    appId="711331871134355"
+    
+  />,
       <div className="container mx-auto px-4 md:px-8 lg:px-16 ">
         <Navbar></Navbar>
       </div>
